bc0f01a25735f42b6377c9f3c70d5e07a668f7fe
Rimo1129 <petrichor.flavor@gmail.com>
2020-06-28 08:18:49 +0900

    Modified templates to display posts from database.

--- atsumori/views.py
+++ atsumori/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import MoriUser
 
 def user_list(request):
-    return render(request, 'atsumori/user_list.html', {})
+    moriusers = MoriUser.objects.filter(fixed_date__lte=timezone.now()).order_by('fixed_date')
+    return render(request, 'atsumori/user_list.html', {'moriusers': moriusers})
